015c1e23e461a1ab2a6bf0e447d29f820d4cb8c9
omkarmanjare <omkarmanjare8311@gmail.com>
2024-10-28 07:16:32 +0530

    added

---
+++ AsyncAwait.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/
+//1. asynchronous method has return type Task [for method that does async operation but does not return anything
+// and Task<T> for methods that does async operation and returns soemthing in the response
+
+
+namespace ObjectOrientedConcepts
+{
+    public class AsyncAwait
+    {
+
+    }
+}
